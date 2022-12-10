97407a3e5763fcb93c5b3218fbfc68c3a3d541e0
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-12-10 17:49:46 +0300

    я сделала бегающую кнопку!

---
+++ knopka/script.js
@@ -0,0 +1,13 @@
+const random = (min, max) => {
+    const rand = min + Math.random() * (max - min + 1);
+    return Math.floor(rand);
+}
+const btn = document.querySelector('#btn');
+btn.addEventListener('mouseenter', () =>{
+    btn.style.left = `${random(0, 90)}%`;
+    btn.style.top = `${random(0, 90)}%`;
+});
+
+btn.addEventListener('click', () => {
+    alert('poop :)')
+});
